7bfd21a33cd3878d2e058c87d6150811c334c1d4
dd-han <han@dd-han.tw>
2017-10-24 20:53:22 +0800

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace radius_manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
